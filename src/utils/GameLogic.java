7302c607c274ec547ec855ce11e8be3ca38f16ea
Yarko <yarko3@hotmail.com>
2015-02-09 13:20:25 -0800

    added skeleton

---
+++ src/utils/GameLogic.java
@@ -0,0 +1,44 @@
+package utils;
+
+public class GameLogic 
+{
+	
+	public static void main(String[] args)
+	{
+		//initialize board
+		
+		//initialize gui
+		
+		//make connection
+		//choose room
+		//see whose turn it is
+		
+		
+		
+	}
+	
+	public void playGame()
+	{
+		//while game is not over
+		
+		
+			// if not our turn
+				// 
+				// calculate potential moves
+				// wait for message
+				// check for end game status
+					// alert team
+				// set to our turn
+		
+			// else
+				// check opponent move legality
+					//if illegal
+						// send error message
+				// calculate move
+				// check for end game status
+					// alert team
+				// send move
+				// set to their move
+		
+	}
+}
