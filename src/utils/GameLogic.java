a8697a0c3fc4100ca6236d45654f46353b0c1e10
ethan <ethan@clandestine>
2015-02-09 23:35:21 -0800

    Moved back to seperate class, refactored some code

--- src/utils/GameLogic.java
+++ src/utils/GameLogic.java
@@ -29,31 +29,26 @@ public class GameLogic implements GamePlayer
 	int roomId;
 	static ArrayList<GameRoom> roomList;
 
-	static Action action;
-	static JAXBContext jaxbContext;
-
 	public static void main(String[] args) throws JAXBException
 	{
-		jaxbContext = JAXBContext.newInstance(Action.class);
 		//gamelogic gamelogic = new gamelogic("team rocket","password123");
 
 		String msg = "<action type='room-joined'><usrlist ucount='1'><usr name='team rocket' id='1'></usr></usrlist></action>";
 		String msg2 = "<action type='move'> <queen move='a3-g3'></queen><arrow move='h4'></arrow></action>";
 
+		OurPair<Integer, Integer> InitialQ = new OurPair<Integer, Integer>(0,0);
+		OurPair<Integer, Integer> FinalQ = new OurPair<Integer, Integer>(5,5);
+		OurPair<Integer, Integer> Arrow = new OurPair<Integer, Integer>(5,2);
+
 		Message message = new Message();
 		message.unmarshal(msg);
-		System.out.println(message.marshal());
-//
-//		Message message1 = new Message();
-//		message1.unmarshal(msg2);
-//		OurPair<Integer, Integer> ourPair = new OurPair<Integer, Integer>(6,7);
-//		OurPair<Integer, Integer> ourPair1 = new OurPair<Integer, Integer>(0,5);
-//		message1.setArrow(ourPair);
-//		message1.setMove(ourPair, ourPair1);
-//		System.out.println(message1.getArrow());
-//
-//		System.out.println(message1.marshal());
 
+//		message.setType("move");
+//		message.setMove(InitialQ, FinalQ);
+//		message.setArrow(Arrow);
+
+		System.out.println(message.getArrow());
+		System.out.println(message.marshal());
 	}
 
 	public GameLogic(String name, String passwd)
@@ -105,36 +100,22 @@ public class GameLogic implements GamePlayer
 		}
 	}
 
-	//general message from the server
 	public boolean handleMessage(String arg0) throws Exception {
 		String msg = XMLParser.parseXML(arg0);
-		//at least print out the message so the other team can see you've timed out
 		System.out.print(arg0);
 		return true;
 	}
 
-	//game-specific message from the server
 	public boolean handleMessage(GameMessage arg0) throws Exception {
 		System.out.println("[SimplePlayer: The server said =]  " + arg0.toString());
 		return true;
 	}
 
-	// You may want to implement a method like this as a central point for sending messages
-	// to the server.
 	public void sendToServer(String msgType, int roomID){
-		// before sending the message to the server, you need to (1) build the text of the message
-		// as a string
 		String msg = "Message goes here";
 		boolean isMove = true;
-		//(2) compile the message by calling
-		// the static method ServerMessage.compileGameMessage(msgType, roomID, actionMsg),
-
 		ServerMessage.compileGameMessage(msgType, roomId, msg);
-		// and (3) call the method gameClient.sendToServer() to send the compiled message.
-
-
 		gameClient.sendToServer(msg, isMove);
-		// For message types and message format, see the GameMessage API and the project notes
 	}
 
 	public void playGame() {
@@ -159,146 +140,6 @@ public class GameLogic implements GamePlayer
 			// send move
 			// set to their move
 
-		}
-	}
-
-	/**
-	 *
-	 * @param msg Reads in an XML string from the game server and unmarshalls
-	 *            it into the object mappings
-	 * @throws JAXBException
-	 */
-
-	public static void unmarshal(String msg) throws JAXBException {
-		InputStream is = new ByteArrayInputStream(msg.getBytes());
-		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-		action = (Action) unmarshaller.unmarshal(is);
-	}
-
-	/**
-	 *
-	 * @return String which is the XML formatting for the response message
-	 *              to the server
-	 * @throws JAXBException
-	 */
-	public static String marshal() throws JAXBException {
-		OutputStream os = new ByteArrayOutputStream();
-		Marshaller marshaller = jaxbContext.createMarshaller();
-		marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
-		marshaller.marshal(action, os);
-
-		return os.toString();
-	}
-
-	/**
-	 * Root of the XML string
-	 */
-	@XmlRootElement(name="action")
-	@XmlAccessorType(XmlAccessType.FIELD)
-	public static class Action
-	{
-		@XmlAttribute(name="type")
-		String type;
-		@XmlElement(name="usrlist")
-		private UserList userList;
-		@XmlElement(name="queen")
-		private Queen queen;
-		@XmlElement(name="arrow")
-		private Arrow arrow;
-	}
-
-	/**
-	 * List of Users Element
-	 */
-	@XmlAccessorType(XmlAccessType.FIELD)
-	public static class UserList
-	{
-		@XmlAttribute(name="ucount")
-		int ucount;
-		@XmlElement(name="usr")
-		List<User> users;
-	}
-
-	/**
-	 * User element and attributs
-	 * None of the examples in the
-	 * message format guide include anything
-	 * except attributes
-	 */
-	@XmlAccessorType(XmlAccessType.FIELD)
-	public static class User
-	{
-		@XmlAttribute(name="name")
-		String name;
-		@XmlAttribute(name="id")
-		int id;
-		@XmlAttribute(name="role")
-		String role;
-	}
-
-	/**
-	 * Queen, stores a move in a string of the
-	 * format previous-current such as g7-a4.
-	 * Is not stored in our internal game board representation
-	 * See getMove for getter of the newest move
-	 */
-	@XmlAccessorType(XmlAccessType.FIELD)
-	public static class Queen
-	{
-		@XmlAttribute(name="move")
-		static String move;
-		/**
-		 *
-		 * @param InitialQ Where the queen started in our turn
-		 * @param FinalQ Where the queen was moved to in our turn
-		 *
-		 */
-		public void setMove(OurPair<Integer, Integer> InitialQ, OurPair<Integer, Integer> FinalQ)
-		{
-			String s1 = new StringBuilder().append(Character.toChars(InitialQ.getX() + 'a')).append(Character.toChars(InitialQ.getY() + '0')).toString();
-			String s2 = new StringBuilder().append(Character.toChars(FinalQ.getX() + 'a')).append(Character.toChars(FinalQ.getY() + '0')).toString();
-
-			this.move = s1 + "-" + s2;
-		}
-
-		/**
-		 *
-		 * @return The location that the queen moved to
-		 */
-		public OurPair<Integer, Integer> getMove(){
-			String[] str = Queen.move.split("-");
-			return new OurPair<Integer, Integer>(str[1].charAt(0)-'a', str[1].charAt(1)-'0');
-		}
-
-	}
-
-	/**
-	 * Stores location that the arrow was fired after
-	 * the queen made her move
-	 */
-	@XmlAccessorType(XmlAccessType.FIELD)
-	public static class Arrow
-	{
-		@XmlAttribute(name="move")
-		static String arrow;
-
-
-		/**
-		 *
-		 * @return location that the arrow was placed
-		 */
-		public OurPair<Integer, Integer> getArrow()
-		{
-			return new OurPair<Integer, Integer>(this.arrow.charAt(0)-'a', this.arrow.charAt(1)-'0');
-		}
-
-		/**
-		 *
-		 * @param arrow takes OurPair object for location that we placed our arrow
-		 */
-		public void setArrow(OurPair<Integer, Integer> arrow)
-		{
-			this.arrow = new StringBuilder().append(Character.toChars(arrow.getX() + 'a')).append(Character.toChars(arrow.getY() + '0')).toString();
 		}
 	}
 }
--- src/utils/Message.java
+++ src/utils/Message.java
@@ -15,12 +15,11 @@ import java.util.List;
 
 /**
  * Created by ethan on 09/02/15.
- *
+ * <p/>
  * This class uses Jaxb to automatically map incoming
  * XML strings to object mappings and will then
  * convert a mapping back to XML string to be sent
  * on the wire.
- *
  */
 public class Message {
     static Action action;
@@ -28,14 +27,15 @@ public class Message {
 
     /**
      * Constructor
+     *
      * @throws JAXBException
      */
     public Message() throws JAXBException {
         jaxbContext = JAXBContext.newInstance(Action.class);
+        action = new Action();
     }
 
     /**
-     *
      * @param msg Reads in an XML string from the game server and unmarshalls
      *            it into the object mappings
      * @throws JAXBException
@@ -48,9 +48,8 @@ public class Message {
     }
 
     /**
-     *
      * @return String which is the XML formatting for the response message
-     *              to the server
+     * to the server
      * @throws JAXBException
      */
     public static String marshal() throws JAXBException {
@@ -65,30 +64,52 @@ public class Message {
     /**
      * Root of the XML string
      */
-    @XmlRootElement(name="action")
+    @XmlRootElement(name = "action")
     @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Action
-    {
-        @XmlAttribute(name="type")
-        String type;
-        @XmlElement(name="usrlist")
-        private UserList userList;
-        @XmlElement(name="queen")
-        private Queen queen;
-        @XmlElement(name="arrow")
-        private Arrow arrow;
+    public static class Action {
+        @XmlAttribute(name = "type")
+        static String type = new String();
+        @XmlElement(name = "usrlist")
+        static UserList userList = new UserList();
+        @XmlElement(name = "queen")
+        static Queen queen = new Queen();
+        @XmlElement(name = "arrow")
+        static Arrow arrow = new Arrow();
+    }
+
+    public String getType() {
+        return Action.type;
+    }
+
+    public void setType(String type) {
+        Action.type = type;
     }
 
     /**
      * List of Users Element
      */
     @XmlAccessorType(XmlAccessType.FIELD)
-    public static class UserList
-    {
-        @XmlAttribute(name="ucount")
-        int ucount;
-        @XmlElement(name="usr")
+    public class UserList {
+        @XmlAttribute(name = "ucount")
+        int ucount = 0;
+        @XmlElement(name = "usr")
         List<User> users;
+    }
+
+    public int getUcount() {
+        return UserList.ucount;
+    }
+
+    public void setUcount(int ucount) {
+        UserList.ucount = ucount;
+    }
+
+    public List<User> getUsers() {
+        return UserList.users;
+    }
+
+    public void setUsers(List<User> users) {
+        UserList.users = users;
     }
 
     /**
@@ -98,15 +119,39 @@ public class Message {
      * except attributes
      */
     @XmlAccessorType(XmlAccessType.FIELD)
-    public static class User
-    {
-        @XmlAttribute(name="name")
-        String name;
-        @XmlAttribute(name="id")
-        int id;
-        @XmlAttribute(name="role")
-        String role;
+    public static class User {
+        @XmlAttribute(name = "name")
+        static String name;
+        @XmlAttribute(name = "id")
+        static int id;
+        @XmlAttribute(name = "role")
+        static String role;
     }
+
+    public String getName() {
+        return User.name;
+    }
+
+    public void setName(String name) {
+        User.name = name;
+    }
+
+    public int getId() {
+        return User.id;
+    }
+
+    public void setId(int id) {
+        User.id = id;
+    }
+
+    public String getRole() {
+        return User.role;
+    }
+
+    public void setRole(String role) {
+        User.role = role;
+    }
+
 
     /**
      * Queen, stores a move in a string of the
@@ -115,31 +160,17 @@ public class Message {
      * See getMove for getter of the newest move
      */
     @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Queen
-    {
-        @XmlAttribute(name="move")
+    public static class Queen {
+        @XmlAttribute(name = "move")
         static String move;
     }
 
     /**
-     * Stores location that the arrow was fired after
-     * the queen made her move
-     */
-    @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Arrow
-    {
-        @XmlAttribute(name="move")
-        static String arrow;
-    }
-
-    /**
-     *
      * @param InitialQ Where the queen started in our turn
-     * @param FinalQ Where the queen was moved to in our turn
-     *
+     * @param FinalQ   Where the queen was moved to in our turn
      */
-    public void setMove(OurPair<Integer, Integer> InitialQ, OurPair<Integer, Integer> FinalQ)
-    {
+    public void setMove(OurPair<Integer, Integer> InitialQ, OurPair<Integer, Integer> FinalQ) {
+        // Take our internal representation of the game board and transform it into the alphanumeric representation that the server expects
         String s1 = new StringBuilder().append(Character.toChars(InitialQ.getX() + 'a')).append(Character.toChars(InitialQ.getY() + '0')).toString();
         String s2 = new StringBuilder().append(Character.toChars(FinalQ.getX() + 'a')).append(Character.toChars(FinalQ.getY() + '0')).toString();
 
@@ -147,29 +178,37 @@ public class Message {
     }
 
     /**
-     *
      * @return The location that the queen moved to
      */
-    public OurPair<Integer, Integer> getMove(){
+    public OurPair<Integer, Integer> getMove() {
         String[] str = Queen.move.split("-");
-        return new OurPair<Integer, Integer>(str[1].charAt(0)-'a', str[1].charAt(1)-'0');
+        // Transform the server-required alphanumeric position representation into our internal representation
+        return new OurPair<Integer, Integer>(str[1].charAt(0) - 'a', str[1].charAt(1) - '0');
     }
 
     /**
-     *
+     * Stores location that the arrow was fired after
+     * the queen made her move
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Arrow {
+        @XmlAttribute(name = "move")
+        static String arrow;
+    }
+
+    /**
      * @param arrow takes OurPair object for location that we placed our arrow
      */
-    public void setArrow(OurPair<Integer, Integer> arrow)
-    {
+    public void setArrow(OurPair<Integer, Integer> arrow) {
+        // Transform our internal arrow position into the external alphanumeric representation
         Arrow.arrow = new StringBuilder().append(Character.toChars(arrow.getX() + 'a')).append(Character.toChars(arrow.getY() + '0')).toString();
     }
 
     /**
-     *
      * @return location that the arrow was placed
      */
-    public OurPair<Integer, Integer> getArrow()
-    {
-        return new OurPair<Integer, Integer>(Arrow.arrow.charAt(0)-'a', Arrow.arrow.charAt(1)-'0');
+    public OurPair<Integer, Integer> getArrow() {
+        // Transform the stored alphanumeric representation into our internal representation
+        return new OurPair<Integer, Integer>(Arrow.arrow.charAt(0) - 'a', Arrow.arrow.charAt(1) - '0');
     }
 }
